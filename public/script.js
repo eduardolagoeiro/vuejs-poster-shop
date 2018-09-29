db1ecb05a569babf865946d5cc5a2afc1ed8fe27
lagoeiro <eduardolagoeiro@yahoo.com.br>
2018-09-29 14:34:41 -0300

    convert score to price

--- public/script.js
+++ public/script.js
@@ -1,15 +1,12 @@
 new Vue({
   el: '#app',
   data:{
+    searched: '',
     search: '',
     searchError: '',
     searching: false,
     total: 0,
-    items: [
-      {title: 'Item 1', value: 1.01},
-      {title: 'Item 2', value: 2.02},
-      {title: 'Item 3', value: 3.03}
-    ],
+    items: [],
     cart: new Map()
   },
   methods:{
@@ -24,40 +21,44 @@ new Vue({
       this.$http
         .get('/search/'.concat(this.search))
         .then(function(resp){
-          console.log(resp.data)
+          console.log(resp.data);
+          this.items = resp.data;
+          this.searched = this.search;
           this.search = '';
           this.searching = false;
         })
         .catch( err => {
           console.log(err);
           this.searching = false;
+          this.searched = '';
+          this.search = '';
         });
     },
     addItem: function(index){
       var itemDetail = this.cart.get(this.items[index]);
       if(itemDetail){
-        incrementBy(itemDetail, 1, this.items[index].value);
+        incrementBy(itemDetail, 1, this.items[index].score/100);
         setItemInCart(this.cart, this.items[index], itemDetail, this);
       }else{
         setItemInCart(this.cart, this.items[index], {
           times: 1,
-          value: this.items[index].value
+          value: this.items[index].score/100
         }, this);
       }
     },
     incrementItem: function(item){
       var itemDetail = this.cart.get(item);
-      incrementBy(itemDetail, 1, item.value);
+      incrementBy(itemDetail, 1, item.score/100);
       setItemInCart(this.cart, item, itemDetail, this);
     },
     decrementItem: function(item){
       var itemDetail = this.cart.get(item);
-      incrementBy(itemDetail, -1, item.value);
+      incrementBy(itemDetail, -1, item.score/100);
       setItemInCart(this.cart, item, itemDetail, this);
     },
     removeItem: function(item){
       var itemDetail = this.cart.get(item);
-      incrementBy(itemDetail, -1*itemDetail.times, item.value);
+      incrementBy(itemDetail, -1*itemDetail.times, item.score/100);
       setItemInCart(this.cart, item, itemDetail, this);
     }
   },
